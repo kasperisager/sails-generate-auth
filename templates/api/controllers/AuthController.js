ca2be661781631547e1921c1fd3391fc973f1647
Dan Kaplun <dan@beardtree.com>
2014-05-22 09:51:01 -0500

    Redirects back to registration/login page if passport.callback throws an error

--- templates/api/controllers/AuthController.js
+++ templates/api/controllers/AuthController.js
@@ -118,18 +118,22 @@ var AuthController = {
    * @param {Object} res
    */
   callback: function (req, res) {
+    function tryAgain () {
+      // If an error was thrown, redirect the user to the login which should
+      // take care of rendering the error messages.
+      req.flash('form', req.body);
+      res.redirect(req.param('action') === 'register' ? '/register' : '/login');
+    }
+
     passport.callback(req, res, function (err, user) {
-      req.login(user, function (err) {
-        // If an error was thrown, redirect the user to the login which should
-        // take care of rendering the error messages.
-        if (err) {
-          res.redirect(req.param('action') === 'register' ? '/register' : '/login');
-        }
+      if (err) return tryAgain();
+
+      req.login(user, function (loginErr) {
+        if (loginErr) return tryAgain();
+
         // Upon successful login, send the user to the homepage were req.user
         // will available.
-        else {
-          res.redirect('/');
-        }
+        res.redirect('/');
       });
     });
   }
