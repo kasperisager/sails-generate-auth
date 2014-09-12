46c532d9c842b8f31c4743140c7203fbe699f502
Travis Webb <me@traviswebb.com>
2014-09-12 05:41:55 -0400

    added createUser function

--- templates/api/services/protocols/local.js
+++ templates/api/services/protocols/local.js
@@ -1,5 +1,3 @@
-var validator = require('validator');
-
 /**
  * Local Authentication Protocol
  *
@@ -13,11 +11,6 @@ var validator = require('validator');
  */
 
 /**
- * Register a new user
- *
- * This method creates a new user from a specified email, username and password
- * and assign the newly created user a local Passport.
- *
  * @param {Object}   req
  * @param {Object}   res
  * @param {Function} next
@@ -42,32 +35,49 @@ exports.register = function (req, res, next) {
     return next(new Error('No password was entered.'));
   }
 
-  User.create({
-    username : username
-  , email    : email
-  }, function (err, user) {
+  exports.createUser({
+    username: username,
+    email: email,
+    password: password
+  }, next);
+};
+
+/**
+ * Register a new user
+ *
+ * This method creates a new user from a specified email, username and password
+ * and assign the newly created user a local Passport.
+ *
+ * @param {String}   username
+ * @param {String}   email
+ * @param {String}   password
+ * @param {Function} next
+ */
+exports.createUser = function (_user, next) {
+  return User.create(_user, function (err, user) {
     if (err) {
       if (err.code === 'E_VALIDATION') {
+        sails.log(err);
         if (err.invalidAttributes.email) {
-          req.flash('error', 'Error.Passport.Email.Exists');
+          throw new Error('Error.Passport.Email.Exists');
         } else {
-          req.flash('error', 'Error.Passport.User.Exists');
+          throw new Error('Error.Passport.User.Exists');
         }
       }
-
+      
       return next(err);
     }
 
     Passport.create({
       protocol : 'local'
-    , password : password
+    , password : _user.password
     , user     : user.id
     }, function (err, passport) {
       if (err) {
         if (err.code === 'E_VALIDATION') {
-          req.flash('error', 'Error.Passport.Password.Invalid');
+          throw new Error('Error.Passport.Password.Invalid');
         }
-
+        
         return user.destroy(function (destroyErr) {
           next(destroyErr || err);
         });
@@ -129,7 +139,7 @@ exports.connect = function (req, res, next) {
  * @param {Function} next
  */
 exports.login = function (req, identifier, password, next) {
-  var isEmail = validator.isEmail(identifier)
+  var isEmail = isEmail(identifier)
     , query   = {};
 
   if (isEmail) {
@@ -179,3 +189,15 @@ exports.login = function (req, identifier, password, next) {
     });
   });
 };
+
+var EMAIL_REGEX = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i;
+
+/**
+ * Use validator module isEmail function
+ *
+ * @see <https://github.com/chriso/validator.js/blob/3.18.0/validator.js#L38>
+ * @see <https://github.com/chriso/validator.js/blob/3.18.0/validator.js#L141-L143>
+ */
+function isEmail (str) {
+  return EMAIL_REGEX.test(str);
+}
