8079e62ea9c427153be19080493944a2b8a760f9
adicirstei <adicirstei@gmail.com>
2015-03-20 16:04:49 +0200

    In sails 0.11 findById returns and array
    In sails 0.11 `findById` returns and array. One might expect to be able to access the user model by `req.user` instead of `req.user[0]`

--- templates/api/services/protocols/bearer.js
+++ templates/api/services/protocols/bearer.js
@@ -13,7 +13,7 @@ exports.authorize = function(token, done) {
   Passport.findOne({ accessToken: token }, function(err, passport) {
     if (err) { return done(err); }
     if (!passport) { return done(null, false); }
-    User.findById(passport.user, function(err, user) {
+    User.findOneById(passport.user, function(err, user) {
       if (err) { return done(err); }
       if (!user) { return done(null, false); }
       return done(null, user, { scope: 'all' });
